b84ed40f6d0f3719e13781690ef77e4d26324452
youss <heloui@et.esiea.fr>
2025-05-15 15:04:35 +0200

    First commit

---
+++ chatbotApp/src/test/java/project/chatbotApp/ChatbotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.chatbotApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
